d79f2d4561776a325989b4fe8ccf49008cf25306
YehudaGoldshtein <yehudago@post.bgu.ac.il>
2024-12-13 13:18:56 +0200

    13.12 13:18

--- alpha_beta_tictacfive.py
+++ alpha_beta_tictacfive.py
@@ -1,13 +1,35 @@
 import math
 
 
-def alphabeta_max(current_game):
-    #add code here for alpha-beta
-    pass
+def alphabeta_max(current_game, alpha=-math.inf, beta=math.inf):
+    if current_game.is_terminal():
+        return current_game.get_score(), None
+    v = -math.inf
+    moves = current_game.get_moves()
+    for move in moves:
+        mx, next_move = alphabeta_min(move, alpha, beta)
+        if v < mx:
+            v = mx
+            best_move = move
+        if v >= beta:
+            return v, best_move
+        alpha = max(alpha, v)
+    return v, best_move
 
-def alphabeta_min(current_game):
-    #add code here for alpha-beta
-    pass
+def alphabeta_min(current_game, alpha=-math.inf, beta=math.inf):
+    if current_game.is_terminal():
+        return current_game.get_score(), None
+    v = math.inf
+    moves = current_game.get_moves()
+    for move in moves:
+        mx, next_move = alphabeta_max(move, alpha, beta)
+        if v > mx:
+            v = mx
+            best_move = move
+        if v <= alpha:
+            return v, best_move
+        beta = min(beta, v)
+    return v, best_move
 
 
 def maximin(current_game):
--- heuristics.py
+++ heuristics.py
@@ -1,7 +1,36 @@
+import numpy as np
+def get_sequences_as_strings(grid):
+    strings = []
+    rows, cols = grid.shape
+    for i in range(rows):
+        strings.append(''.join(str(x) for x in grid[i, :]))
+    for i in range(cols):
+        strings.append(''.join(str(x) for x in grid[:, i]))
+    for i in range(rows):
+        strings.append(''.join(str(x) for x in grid.diagonal(i)))
+        strings.append(''.join(str(x) for x in np.fliplr(grid).diagonal(i)))
+    return strings
+
+def get_all_sequences(grid, player, length):
+    #return sequences of length 'length' that contain only 'player' in the grid that has a zero before or after the sequence
+    #sequences can be horizontal, vertical or diagonal
+    other_player = 1 if player == 2 else 2
+    p1_moves, p2_moves = 0, 0
+    strings = get_sequences_as_strings(grid)
+    string_formats_for_player = {str(player) * length + '0', '0' + str(player) * length}
+    string_formats_for_other_player = {str(other_player) * length + '0', '0' + str(other_player) * length}
+    for string in strings:
+        if string in string_formats_for_player:
+            p1_moves += 1
+        if string in string_formats_for_other_player:
+            p2_moves += 1
+    return p1_moves - p2_moves
+
 
 def base_heuristic(curr_state):
-    #add code here
-    pass
+    grid = curr_state.get_grid()
+    player = curr_state.get_curr_player()
+    return get_all_sequences(grid, player, curr_state.winning_length)
 
 def advanced_heuristic(curr_state):
-    return 0
+    return base_heuristic(curr_state)
